d977613cc7681e4ccf669c6d5c30a102fba0f07c
torofms37 <torofms37@gmail.com>
2024-11-12 17:10:08 -0700

    cosas

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 function toggleTheme() {
   const body = document.body;
   const themeToggleBtn = document.querySelector('.theme-toggle');
-  const svgMoon = document.getElementById('moon'); 
-  const svgSun = document.getElementById('light');
-  
+  const svgMoon = document.getElementById('moon');
+  const svgSun = document.getElementById('sun');
+
   // Cambia entre temas
   body.classList.toggle('dark-theme');
   body.classList.toggle('light-theme');
-  
-  // Actualiza el texto del botón
+
+  // Actualiza el contenido del botón
   if (body.classList.contains('dark-theme')) {
-    svgMoon.style.display = 'none'; 
-    svgSun.style.display = 'block';
+      themeToggleBtn.innerHTML = ''; // Limpia el contenido del botón
+      const sunClone = svgSun.cloneNode(true);
+      sunClone.style.display = 'inline'; // Asegúrate de que el clon esté visible
+      themeToggleBtn.appendChild(sunClone); // Añade el SVG del sol
   } else {
-    svgSun.style.display = 'block';
-    svgMoon.style.display = 'none'; 
+      themeToggleBtn.innerHTML = ''; // Limpia el contenido del botón
+      const moonClone = svgMoon.cloneNode(true);
+      moonClone.style.display = 'inline'; // Asegúrate de que el clon esté visible
+      themeToggleBtn.appendChild(moonClone); // Añade el SVG de la luna
   }
 }
